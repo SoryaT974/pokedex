38be1af9eb51718121cf7598f2f598fa6d9ab7ea
sorya <sya0974@gmail.com>
2021-10-12 14:07:39 +0000

    table seeder

---
+++ pokedex/database/seeders/MoveSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MoveSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('moves')->insert(
+            [
+                'id' => 1,
+                'name' => 'pokemon1',
+                'type_id' => 1,
+                'power' => 20,
+                'accuracy' => 80,
+                'created_at' => now()
+            ]
+           
+        );
+    }
+}
---
+++ pokedex/database/seeders/PokemonSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PokemonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('pokemons')->insert(
+            [
+                'id' => 1,
+                'name' => 'pokemon1',
+                'type_id' => 1,
+                'image' => 'https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.pokepedia.fr%2Fimages%2F8%2F8b%2F%25C3%2589voli-RFVF.png%3Ffbclid%3DIwAR3SUl7UAJMwnlQbexVoMFccKPKsgLbY9Zd5vbiG4qdRKanMOdHSPnu3Ntk&h=AT0LSjvx8YGYMcOiRLCVGFkC54WT6Hc7sHDYBI_fvn4nlGQkb7xvNYsfPO6bMroZkK-WoTwvVf0Lh6F_Gnhvjkd88N0B37YHvMGQbKRYeVL5qgBD_9h1nsio3rXOH0aIYxITLg-KhUgKiOGck1aSKQ',
+                'created_at' => now()
+            ]
+           
+        );
+    }
+}
+
+
